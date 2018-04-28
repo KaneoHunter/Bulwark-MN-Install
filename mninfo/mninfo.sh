cb48ca3fdda1ba0d5422507495025a5dc43305c6
Kaneo <dingdongkane@gmail.com>
2018-04-28 21:59:01 +0100

    Revert "Merge branch 'mninfo' into master"
    This reverts commit 1687fbd60d087feb34861f3a14dec10c3339b553, reversing
    changes made to f704299d584b01238c5c7b8f9cda44cf555c141f.

--- mninfo/mninfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-touch ~/.bulwark/hourly_status.txt
-date '+%A %W %Y %X' >> ~/.bulwark/hourly_status.txt
-bulwark-cli getblockchaininfo >> ~/.bulwark/hourly_status.txt
-bulwark-cli masternode status >> ~/.bulwark/hourly_status.txt
-bulwark-cli getinfo >> ~/.bulwark/hourly_status.txt
-echo "" >> ~/.bulwark/hourly_status.txt
