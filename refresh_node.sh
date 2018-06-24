48fc581a38463dc681c5f52ec3a3beb2707ca91b
KaneoHunter <dingdongkane@gmail.com>
2018-06-24 03:33:40 +0100

    With a little help from my friends.
    Lend me your ears and I'll sing you a song and I'll try not to sing out of key
    oh I get by little help from my friends
    oh I get high with a little help from my friends
    oh I'm gonna try with a little help from my friends

--- refresh_node.sh
+++ refresh_node.sh
@@ -48,11 +48,7 @@ clear
 echo "Your masternode is syncing. Please wait for this process to finish."
 echo "This can take up to a few hours. Do not close this window." && echo ""
 
-<<<<<<< HEAD
 until [ -n "$(bulwark-cli getconnectioncount 2>/dev/null)"  ]; do
-=======
-until [  $(bulwark-cli getconnectioncount) -gt 0  ] >/dev/null; do
->>>>>>> Update refresh_node.sh
   sleep 1
 done
 
